f869d8ac2807a2f667b39d2054bf7da56aa1c14c
Tim Pundt <tim.pundt@trojans.dsu.edu>
2023-12-02 00:52:09 -0700

    Almost done

--- 26.py
+++ 26.py
@@ -1,15 +1,23 @@
 def main():
-    nums = [0,0,1,1,1,2,2,3,3,4]
-    updated = []
+    nums = [1, 1, 2]
 
+    if len(set(nums)) == 1:
+        return 1
+
+    for k in range(0, len(nums)):
+        if k != len(nums)-1:
+            if nums[k] == nums[k+1] or nums[k] == nums[k-1]:
+                nums.remove(nums[k])
+                nums.append("_")
+
+    x = 0
     for value in nums:
-        if value not in updated:
-            updated.append(value)
+        if value != "_":
+            x += 1
 
-    print(f"{updated}")
+
+    print(f"{nums} and {x}")
 
 
 if __name__ == '__main__':
     main()
-
-
