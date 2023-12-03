fd7e00f36bd338881d553d1a57dcd721ff8ef824
Tim Pundt <tim.pundt@trojans.dsu.edu>
2023-12-03 15:33:28 -0700

    Optimal 2 ptr solution after seeing a submission of it

--- 26.py
+++ 26.py
@@ -1,26 +1,13 @@
 def main():
     nums = [1,1,2]  # bad algorithm, just start over and do a bubble sort method instead
 
-    if len(set(nums)) == 1:
-        return 1
+    k = 1
+    for i in range(1, len(nums)):
+        if nums[i] != nums[i - 1]:
+            nums[k] = nums[i]
+            k += 1
 
-    for y in range(0, 8):
-        for k in range(0, len(nums)):
-            if k <= len(nums) - 2:
-                if nums[k] == nums[k + 1] or nums[k] == nums[k - 1]:
-                    nums.remove(nums[k])
-                    nums.insert(nums[k], 1000)
-        nums.sort()
-        k = 0
-
-    nums.sort()
-
-    x = 0
-    for value in nums:
-        if value != 1000:
-            x += 1
-
-    print(f"{nums} and {x}")
+    print(f"{nums} and {k}")
 
 
 if __name__ == '__main__':
