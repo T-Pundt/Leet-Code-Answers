5cfd01504771fb2ce290ecae788425fad1f6e596
Tim Pundt <tim.pundt@trojans.dsu.edu>
2023-12-03 14:58:23 -0700

    The problem should be complete

--- 26.py
+++ 26.py
@@ -1,29 +1,25 @@
 def main():
-    nums = [0,0,0,0,3] #bad algorithm, just start over and do a bubble sort method instead
+    nums = [0, 0, 0, 0, 3]  # bad algorithm, just start over and do a bubble sort method instead
 
 
     if len(set(nums)) == 1:
         return 1
 
     for k in range(0, len(nums)):
-        if k != len(nums)-1:
-            if nums[k] == nums[k+1] or nums[k] == nums[k-1]:
+        if k != len(nums) - 1:
+            if nums[k] == nums[k + 1] or nums[k] == nums[k - 1]:
                 nums.remove(nums[k])
-                nums.append("_")
+                nums.insert(nums[k], 1000)
 
-            if nums[k-1] == nums[k] or nums[k-2] == nums[k-1]:
-                nums.remove(nums[k-1])
-                nums.append("_")
+    nums.sort()
+
     x = 0
     for value in nums:
-        if value != "_":
+        if value != 1000:
             x += 1
-
 
     print(f"{nums} and {x}")
 
 
 if __name__ == '__main__':
     main()
-
-
