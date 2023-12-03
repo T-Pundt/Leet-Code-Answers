d355618a6d8d56b9b98e19bf5044e1b7f1eb2dc5
Tim Pundt <tim.pundt@trojans.dsu.edu>
2023-12-03 15:16:47 -0700

    Bad solution, but it works

--- 26.py
+++ 26.py
@@ -1,15 +1,17 @@
 def main():
-    nums = [0, 0, 0, 0, 3]  # bad algorithm, just start over and do a bubble sort method instead
-
+    nums = [1,1,2]  # bad algorithm, just start over and do a bubble sort method instead
 
     if len(set(nums)) == 1:
         return 1
 
-    for k in range(0, len(nums)):
-        if k != len(nums) - 1:
-            if nums[k] == nums[k + 1] or nums[k] == nums[k - 1]:
-                nums.remove(nums[k])
-                nums.insert(nums[k], 1000)
+    for y in range(0, 8):
+        for k in range(0, len(nums)):
+            if k <= len(nums) - 2:
+                if nums[k] == nums[k + 1] or nums[k] == nums[k - 1]:
+                    nums.remove(nums[k])
+                    nums.insert(nums[k], 1000)
+        nums.sort()
+        k = 0
 
     nums.sort()
 
